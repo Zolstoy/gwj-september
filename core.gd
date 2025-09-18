5815a2333c919b6efce5a23233a430be55aff523
Zolstoy <zolstoy.dev@gmail.com>
2025-09-18 11:11:48 +0200

    WIP

--- core.gd
+++ core.gd
@@ -1,7 +1,20 @@
 extends Node
 
-@onready var level = %Level
+@onready var level: RigidBody3D = %Level
+@onready var level_csg: CSGPolygon3D = %Level/CSGPolygon3D
+@onready var level_collision: CollisionShape3D = %Level/CollisionShape3D
+
+var scale = 10
 
 func _ready() -> void:
-	level.scale.x *= 100.
-	level.scale.z *= 100.
+	level_csg.scale_object_local(Vector3(scale, scale, 1))
+	(level_csg.material_override as StandardMaterial3D).uv1_scale.x *= scale
+	(level_csg.material_override as StandardMaterial3D).uv1_scale.y *= scale
+	level_collision.scale_object_local(Vector3(scale, scale, 1))
+
+	var level_part = level.duplicate()
+	
+	level_part.translate(Vector3(0, 0, -scale / 2.))
+	level_part.rotate(Vector3(1, 0, 0), deg_to_rad(90))
+	
+	add_child(level_part)
--- player.gd
+++ player.gd
@@ -1,6 +1,12 @@
-extends Node3D
+extends RigidBody3D
 
-	
-func _input(event: InputEvent) -> void:
-	if event.is_action("Jump"):
-		$RigidBody3D.apply_central_force(Vector3(0, 10, 0))
+
+#func _input(event: InputEvent) -> void:
+	#if event.is_action("Jump"):
+		#apply_central_force(Vector3(0, 100, 0))
+		#
+	#if event.is_action("Forward"):
+		#linear_velocity = Vector3.FORWARD
+
+#func _physics_process(delta: float) -> void:
+	#var collision = move_and_collide(linear_velocity * delta)
